39db78f19e4e58d40d1d1e79fda50d4297860fd5
Xiong-YH <974205071@qq.com>
2022-04-03 21:14:56 +0800

    每日一题（5）

---
+++ 744. 寻找比目标字母大的最小字母.js
@@ -0,0 +1,23 @@
+// // 给你一个排序后的字符列表 letters ，列表中只包含小写英文字母。另给出一个目标字母 target，请你寻找在这一有序列表里比目标字母大的最小字母。
+
+// 在比较时，字母是依序循环出现的。举个例子：
+
+// 如果目标字母 target = 'z' 并且字符列表为 letters = ['a', 'b']，则答案返回 'a'
+//  
+
+// 示例 1：
+
+// 输入: letters = ["c", "f", "j"]，target = "a"
+// 输出: "c"
+
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+ var nextGreatestLetter = function(letters, target) {
+    for(const i of letters) {
+        if (i > target) return i;
+    }
+    return letters[0]
+};
