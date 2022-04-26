7b189aaa8d29600c8526f0372a3fc812c019b9ca
Xiong-YH <974205071@qq.com>
2022-04-26 23:44:38 +0800

    Update 1. 两数之和.js

--- 1. 两数之和.js
+++ 1. 两数之和.js
@@ -33,3 +33,10 @@
         }
     }
 };
+var twoSum = function(nums, target) {
+    for (let i = 0;i < nums.length;i++) {
+        for (let n = i+1;n < nums.length;n++) {
+            if (nums[i] + nums[n] == target) return [i,n]
+        }
+    }
+};
