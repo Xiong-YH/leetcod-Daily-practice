a8c288e10d4e7ad2486dc26829bd6066cc008af5
Xiong-YH <974205071@qq.com>
2022-06-08 20:53:21 +0800

    Create 344.反转字符串.js

---
+++ 344.反转字符串.js
@@ -0,0 +1,28 @@
+// 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 s 的形式给出。
+
+// 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+
+//  
+
+// 示例 1：
+
+// 输入：s = ["h","e","l","l","o"]
+// 输出：["o","l","l","e","h"]
+// 示例 2：
+
+// 输入：s = ["H","a","n","n","a","h"]
+// 输出：["h","a","n","n","a","H"]
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+ var reverseString = function(s) {
+    let i = 0,
+        x = s.length -1;
+    while (i < x) {
+        [s[i], s[x]] = [ s[x], s[i] ]
+        i++
+        x--
+    }
+};
+// 双指针的运用
