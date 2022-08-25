89073d1626b525904e8a5c0a6cfdba3372cde559
Xiong-YH <974205071@qq.com>
2022-08-25 18:19:08 +0800

    Create 160.相交链表.js

---
+++ 160.相交链表.js
@@ -0,0 +1,34 @@
+// 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+
+// 图示两个链表在节点 c1 开始相交：
+// 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+// 输出：Intersected at '8'
+// 解释：相交节点的值为 8 （注意，如果两个链表相交则不能为 0）。
+// 从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,6,1,8,4,5]。
+// 在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+ var getIntersectionNode = function(headA, headB) {
+    if (headA === null || headB === null) {
+    return null;
+  }
+  var curA = headA, curB = headB;
+  while(curA !== curB) {
+    curA = curA !== null ? curA.next : headB;
+    curB = curB !== null ? curB.next : headA;
+  }
+  return curA;
+};
